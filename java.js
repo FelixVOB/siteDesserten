8ab92f9e4735fbe64b6626290097ac5e48e5e0a7
Felix Van Obbergen~ <felix_vanobbergen@hotmail.com>
2022-06-24 22:37:55 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+function sideNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
